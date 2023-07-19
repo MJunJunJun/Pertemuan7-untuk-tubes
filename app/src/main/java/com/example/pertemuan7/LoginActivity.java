e38938518fd68447daf490d58c2fea7d2ef5f9a1
mjuna21 <mjunaedialarnai1@gmail.com>
2023-07-19 20:12:49 +0700

    Anu Nganu

--- app/src/main/java/com/example/pertemuan7/LoginActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.pertemuan7;
-
-import android.app.Activity;
-
-public class LoginActivity extends Activity {
-}
--- app/src/main/java/com/example/pertemuan7/RegisterActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.pertemuan7;
-
-import android.app.Activity;
-
-public class RegisterActivity extends Activity {
-}
